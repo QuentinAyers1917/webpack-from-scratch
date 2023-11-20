3b1ce34805d68fe1b19f1bdf2e2701a39fb87fd7
QuentinAyers1917 <rkowert@posteo.de>
2023-11-20 23:57:59 +0100

    created .babelrc config file for the babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        
+        use: {
+          loader: 'babel-loader' 
+        } 
       }
     ]
   }
